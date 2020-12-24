60fa1b42f95564528ddfb7080d6ce4138e5a9484
Uadson <uadsoncastro@gmail.com>
2020-12-24 12:15:59 -0300

    ex_01 em listas

---
+++ ExercicioListas/ex_01.py
@@ -0,0 +1,14 @@
+'''Faça um Programa que leia um vetor de 5 números inteiros e mostre-os. '''
+
+from random import randint
+
+# lista = [n1, n2, n3, n4, n5]
+lista = [
+    randint(1, 10),
+    randint(11, 20), 
+    randint(21, 30),
+    randint(31, 40),
+    randint(41, 50),
+]
+
+print(lista)
