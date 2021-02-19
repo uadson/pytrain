facdc58ca921995431fc10eb788e1c5c312d3ce7
uadson <uadsoncastro@gmail.com>
2021-02-19 11:57:36 -0300

    added ex 2, 3, 4 and 5

---
+++ EstruturaSequencial/ex_04.py
@@ -0,0 +1,12 @@
+'''
+Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+'''
+
+nota1 = float(input('1ª Nota: '))
+nota2 = float(input('2ª Nota: '))
+nota3 = float(input('3ª Nota: '))
+nota4 = float(input('4ª Nota: '))
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+print(f'A média das notas informadas é {media}.')
---
+++ EstruturaSequencial/ex_05.py
@@ -0,0 +1,7 @@
+'''Faça um Programa que converta metros para centímetros.'''
+
+metros = float(input('Metros: '))
+
+cent = metros * 100
+
+print(f'{metros} metros equivale a {cent} centímetros.')
