ac91093c3d327fb3e9209972a58bf11e6b4fa5d7
uadson <uadsoncastro@gmail.com>
2021-03-01 21:55:23 -0300

    ex_02.py alterado

--- EstruturaSequencial/ex_02.py
+++ EstruturaSequencial/ex_02.py
@@ -1,7 +1,7 @@
-'''Faça um Programa que peça um número e então mostre a mensagem 
-O número informado foi [número].'''
+'''
+Faça um Programa que peça um número e então mostre 
+a mensagem O número informado foi [número].
+'''
 
-
-num = int(input('Informe um número: '))
-
-print(f'O número informado foi [{num}].')
+numero = int(input('Informe um número: '))
+print(f'O número informado foi [{numero}].')
