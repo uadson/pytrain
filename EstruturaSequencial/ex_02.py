b38b99c82fdbd3c80f56bdf526d9745f2b0d254e
uadson <uadsoncastro@gmail.com>
2021-03-01 21:52:43 -0300

    altered ex_02.py

--- EstruturaSequencial/ex_02.py
+++ EstruturaSequencial/ex_02.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 '''Faça um Programa que peça um número e então mostre a mensagem 
 O número informado foi [número].'''
 
@@ -6,12 +5,3 @@ O número informado foi [número].'''
 num = int(input('Informe um número: '))
 
 print(f'O número informado foi [{num}].')
-=======
-'''
-Faça um Programa que peça um número e então mostre 
-a mensagem O número informado foi [número].
-'''
-
-numero = int(input('Informe um número: '))
-print(f'O número informado foi [{numero}].')
->>>>>>> facdc58ca921995431fc10eb788e1c5c312d3ce7
