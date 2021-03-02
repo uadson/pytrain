02b750a706bcbc13caac501fd519d2128ab115f1
uadson <uadsoncastro@gmail.com>
2021-03-01 21:51:24 -0300

    added ex_05.py

--- EstruturaSequencial/ex_02.py
+++ EstruturaSequencial/ex_02.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 '''Faça um Programa que peça um número e então mostre a mensagem 
 O número informado foi [número].'''
 
@@ -5,3 +6,12 @@ O número informado foi [número].'''
 num = int(input('Informe um número: '))
 
 print(f'O número informado foi [{num}].')
+=======
+'''
+Faça um Programa que peça um número e então mostre 
+a mensagem O número informado foi [número].
+'''
+
+numero = int(input('Informe um número: '))
+print(f'O número informado foi [{numero}].')
+>>>>>>> facdc58ca921995431fc10eb788e1c5c312d3ce7
---
+++ EstruturaSequencial/ex_03.py
@@ -0,0 +1,10 @@
+'''
+Faça um Programa que peça dois números e imprima a soma.
+'''
+
+num1 = int(input('Informe o 1º número: '))
+num2 = int(input('Informe o 2º número: '))
+
+soma = num1 + num2
+
+print(f'A soma dos números é {soma}')
