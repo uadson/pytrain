215da3ce3a5b6e8a45e486a5f6e18888ba8c9de2
uadson <uadsoncastro@gmail.com>
2021-03-03 19:11:41 -0300

    added ex_06.py

---
+++ EstruturaSequencial/ex_06.py
@@ -0,0 +1,9 @@
+"""Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+"""
+print('Cálculo da Área de um Círculo\n')
+raio = float(input('Informe o valor do raio: '))
+
+# area = pi * raio
+area = 3.14 * raio
+
+print(f'A área do círculo é: {area:.2f}.')
